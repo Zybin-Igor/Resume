386f6c469a98a530f7b9bc8851103d20cf798616
Igor_job <zibi_man@ukr.net>
2022-01-18 17:22:01 +0200

    add script first lesson

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 "use sctrict";
-console.log("Open Resume");
+//console.log("Open Resume");
+const numberOfFilms = prompt('how many films have you watched?', ' ');
+let PesonalMovieOB = {
+    count:numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+const a = prompt('Last watched movie?', ''),
+      b = prompt('How do you rate?', ''),
+      c = prompt('Last watched movie?', ''),
+      d = prompt('How do you rate?', '');
+PesonalMovieOB.movies[a] = b;
+
